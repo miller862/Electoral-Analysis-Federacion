99de3470227f7a4b96275574d15b9defa9769d04
miller862 <43028862manu@gmail.com>
2023-11-06 16:15:37 -0300

    acomodando

--- codigo.R
+++ codigo.R
@@ -2,8 +2,8 @@ library(xlsx)
 library(readxl)
 library(tidyverse)
 
-#para la primaera mesa, la limpiamos y pivoteamos manual
-
+#esto es codigo viejo
+'''
 mesa_3190 <- read.xlsx("data/mesa_3190.xls",1) %>% 
   .[-c(1:4),]
 
@@ -20,8 +20,8 @@ mesa_3190<- mesa_3190 %>%
                values_to= "VOTOS")
 
 mesa_3190$MESA <- as.double(mesa_3190$MESA)
-
-#DESDE ACA LO ESTANDARIZO  y creo una funcion para ussar de ahora en mas
+'''
+#creo una funcion par alimpiar todas las mesas
 
 limpiar <- function(DF) {
   
@@ -38,9 +38,9 @@ limpiar <- function(DF) {
 
 }
 
-#ahora para el resto de mesas, voy  crear una funcion que lea los excels y luego los paso por la funcion de limpiar
-
-mesa_numbers <- list(3191,3192,3193,3194,3195,3196,3197,3198,3199,3200,3201,3202,3203,3204,3205,3206,3207,3208,3209,3210,3211,3212,3213,3214,3215,3216,3217,3218,3219,3220,3221,3222,3223,3224,3225,3226,3227,3228,3229,3230,3231,3232,3233,3234,3235)
+#voy  crear una funcion que lea los excels y luego los paso por la funcion de limpiar
+#la funcion va a requerir una lista con los numeros de mesas
+mesa_numbers <- list(3190,3191,3192,3193,3194,3195,3196,3197,3198,3199,3200,3201,3202,3203,3204,3205,3206,3207,3208,3209,3210,3211,3212,3213,3214,3215,3216,3217,3218,3219,3220,3221,3222,3223,3224,3225,3226,3227,3228,3229,3230,3231,3232,3233,3234,3235,3236)
 
 ##funcion de lectura
 read_mesa_files <- function(mesa_numbers) {
@@ -71,6 +71,7 @@ read_mesa_files <- function(mesa_numbers) {
 mesa_dfs <-read_mesa_files(mesa_numbers)
 
 #guardo cada mesa
+mesa_3190 <- mesa_dfs[[3190]]
 mesa_3191 <- mesa_dfs[[3191]]
 mesa_3192 <- mesa_dfs[[3192]]
 mesa_3193 <- mesa_dfs[[3193]]
@@ -116,10 +117,12 @@ mesa_3232 <- mesa_dfs[[3232]]
 mesa_3233 <- mesa_dfs[[3233]]
 mesa_3234 <- mesa_dfs[[3234]]
 mesa_3235 <- mesa_dfs[[3235]]
+mesa_3236 <- mesa_dfs[[3236]]
 
 
 #aplico la funcion a todas las mesa
 
+mesa_3190 <- limpiar(mesa_3190)
 mesa_3191 <- limpiar(mesa_3191)
 mesa_3192 <- limpiar(mesa_3192)
 mesa_3193 <- limpiar(mesa_3193)
@@ -165,5 +168,16 @@ mesa_3232 <- limpiar(mesa_3232)
 mesa_3233 <- limpiar(mesa_3233)
 mesa_3234 <- limpiar(mesa_3234)
 mesa_3235 <- limpiar(mesa_3235)
+mesa_3236 <- limpiar(mesa_3236)
 
-todo = bind_rows(mesa_3190,mesa_3191,mesa_3192,mesa_3192, mesa_3193, mesa_3194, mesa_3195, mesa_3196, mesa_3197, mesa_3198, mesa_3199, mesa_3200, mesa_3201, mesa_3202, mesa_3203, mesa_3204, mesa_3205, mesa_3206, mesa_3207, mesa_3208, mesa_3209, mesa_3210, mesa_3211, mesa_3212, mesa_3213, mesa_3214, mesa_3215, mesa_3216, mesa_3217, mesa_3218, mesa_3219, mesa_3220, mesa_3221, mesa_3222, mesa_3223, mesa_3224, mesa_3225, mesa_3226, mesa_3227, mesa_3228, mesa_3229, mesa_3230, mesa_3231, mesa_3232, mesa_3233, mesa_3234, mesa_3235)
+todo = bind_rows(mesa_3190,mesa_3191,mesa_3192, mesa_3193, mesa_3194, mesa_3195, mesa_3196, mesa_3197, mesa_3198, mesa_3199, mesa_3200, mesa_3201, mesa_3202, mesa_3203, mesa_3204, mesa_3205, mesa_3206, mesa_3207, mesa_3208, mesa_3209, mesa_3210, mesa_3211, mesa_3212, mesa_3213, mesa_3214, mesa_3215, mesa_3216, mesa_3217, mesa_3218, mesa_3219, mesa_3220, mesa_3221, mesa_3222, mesa_3223, mesa_3224, mesa_3225, mesa_3226, mesa_3227, mesa_3228, mesa_3229, mesa_3230, mesa_3231, mesa_3232, mesa_3233, mesa_3234, mesa_3235,mesa_3236)
+
+todo <- todo %>% mutate(VOTOS = as.integer(VOTOS))
+
+Blancos_int <- todo %>%
+  filter((`AGRUPACION POLITICA`== 'VOTOS EN BLANCO')&CARGOS== 'PRESIDENTE MUNICIPAL' )
+
+#cantidad de votos blancos para intendente
+Blancos_int%>% summarize(TOTAL= sum(VOTOS))
+
+
